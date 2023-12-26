0b8108bba8ec05ab666d6e2d58f9fa47e0d974cf
Irina Lamarr <irga.lamarr@gmail.com>
2023-12-26 18:09:18 -0500

    task 7-9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -29,7 +29,7 @@ const authenticatedUser = (username,password)=>{
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-    const username = req.body.username;
+  const username = req.body.username;
   const password = req.body.password;
   if (!username || !password) {
       return res.status(404).json({message: "Error logging in"});
@@ -49,8 +49,30 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const review = req.body.review;
+    const username = req.session.authorization.username;
+    if (books[isbn]) {
+        let book = books[isbn];
+        book.reviews[username] = review;
+        return res.status(200).send("Review successfully posted");
+    }
+    else {
+        return res.status(404).json({message: `ISBN ${isbn} not found`});
+    }
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+    if (books[isbn]) {
+        let book = books[isbn];
+        delete book.reviews[username];
+        return res.status(200).send("Review successfully deleted");
+    }
+    else {
+        return res.status(404).json({message: `ISBN ${isbn} not found`});
+    }
 });
 
 module.exports.authenticated = regd_users;
