8b88ca3a11aec41ff8dfea03419f19dd751676b5
Irina Lamarr <irga.lamarr@gmail.com>
2023-12-26 17:29:31 -0500

    task 1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,15 +6,35 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username && password) {
+      if (!isValid(username)) {
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});
+      }
+    }
+    return res.status(404).json({message: "Unable to register user."});
 });
 
+function getBooks() {
+    return new Promise((resolve, reject) => {
+        resolve(books);
+    });
+}
+
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/', async function (req, res) {
+    try {
+      const bks = await getBooks();
+      res.send(JSON.stringify(bks));
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
